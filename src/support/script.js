fd6e5ee258404f42c0af93f3e78d4c4da69134f4
Even Torset <eventorset@gmail.com>
2023-08-01 21:56:05 +0200

    Added easyfundraising donation total and supporters count to the support page.

--- src/support/script.js
+++ src/support/script.js
@@ -11,3 +11,11 @@ const imageObserver = new IntersectionObserver(function(entries, observer) {
 for (const img of document.querySelectorAll('.section img')) {
   imageObserver.observe(img)
 }
+
+try {
+  const cause = await fetch('https://api-v3.easyfundraising.org.uk/causes/e8b381e5b2125a1f43ecd65b8348b3fbb2e600e9').then(e => e.json())
+  document.getElementById('easyfundraising-total').textContent = cause.details.cause.donationTotal
+  document.getElementById('easyfundraising-supporters').textContent = cause.details.cause.numberOfSupporters
+} catch {
+  document.getElementById('easyfundraising-info').remove()
+}
